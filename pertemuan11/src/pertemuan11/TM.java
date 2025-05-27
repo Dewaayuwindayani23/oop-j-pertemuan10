7dfeda1b7450bf0edaaa6291f56665aff07dc073
Dewaayuwindayani <dewayuwindayani@gmail.com>
2025-05-27 22:12:01 +0800

    first commit

---
+++ pertemuan11/src/pertemuan11/TM.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan11;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author LAB F
+ */
+class TM {
+
+    static void setModel(DefaultTableModel DM) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
